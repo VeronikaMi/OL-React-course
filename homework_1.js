7e43d9c62a1fb3d409e49b00ea7ea0d9a7919c83
VeronikaMi <80811438+VeronikaMi@users.noreply.github.com>
2021-12-17 20:55:19 +0400

    Update homework_1.js

--- homework_1.js
+++ homework_1.js
@@ -1,1 +1,27 @@
+let row = 4;
+let col = 5;
 
+const getTable = (row, col) => {
+  let num = 0;
+  let arr = new Array(row);
+  for (let i = 0; i < row; i++) {
+    arr[i] = [];
+  }
+
+  for (let i = 0; i < col; i++) {
+    if (i % 2 === 0) {
+      for (let j = 0; j < row; j++) {
+        arr[j].push(++num);
+      }
+    }
+    else {
+      for (let j = row - 1; j >= 0; j--) {
+        arr[j].push(++num);
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(getTable(row, col));
